7fafb1d5d3246dda4c25d59ca8d027fcb366f0ca
gauthamrokkame <rokkamgauthamreddy@gmail.com>
2025-08-28 03:12:43 +0530

    client: vite deps + npm-exec build; ignore node_modules

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,20 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        secure: false
-      },
-      // 👇 this is the key bit: proxy websockets for Socket.IO
-      '/socket.io': {
-        target: 'http://localhost:4000',
-        ws: true,
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
